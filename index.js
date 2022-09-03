39cee9598457973ebe510967205156988f70ceeb
costagil <eusoude74@mail.com>
2022-09-03 17:45:14 -0300

    :cosmetic: completa form informações profissionais

---
+++ index.js
@@ -0,0 +1,11 @@
+// const btnNextPage = document.querySelector('#next-page')
+// function loudNextPage() {
+//     const personalInfoContainer = document.querySelector('#personal-info-container')
+//     personalInfoContainer.style.display = 'none';
+// }
+// btnNextPage.addEventListener('click', loudNextPage)
+
+
+document.querySelector('#next-page').addEventListener('click', () => {
+    document.querySelector('#personal-info-container').style.display = 'none'; 
+})
