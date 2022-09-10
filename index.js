036670da7e82360ca431efa1c15937e1b631f068
costagil <eusoude74@mail.com>
2022-09-10 17:28:30 -0300

    Refatora evento de submit

--- index.js
+++ index.js
@@ -6,28 +6,47 @@ const btnBackProfInfo = document.querySelector('#btn-back-prof-info');
 const btnBackPageAllInfo = document.querySelector('#btn-back-page-all-info');
 const contPageAllInfo = document.querySelector('#container-page-all-info');
 const footerPageAllInfo = document.querySelector('#footer');
-const inputNome = document.querySelector('#input-nome');
-const inputEmail = document.querySelector('#input-email');
-const inputContato = document.querySelector('#input-contato');
-const inputIdade = document.querySelector('#input-idade');
-const inputCargo = document.querySelector('#input-cargo');
-const scholarity = document.querySelector('#scholarity');
+const personalForm = document.querySelector('#personal-form');
+// const inputNome = document.querySelector('#input-nome');
+// const inputEmail = document.querySelector('#input-email');
+// const inputContato = document.querySelector('#input-contato');
+// const inputIdade = document.querySelector('#input-idade');
+// const inputCargo = document.querySelector('#input-cargo');
+// const scholarity = document.querySelector('#scholarity');
 
-nextPage.addEventListener('click', () => {
-    const sexo = document.querySelector('input[name="sexo"]:checked')
-    // profInfoCont.classList.remove('none');
-    // personalInfCont.classList.add('none');
+
+personalForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const form = event.target
     const personalData = {
-        nome: inputNome.value,
-        email: inputEmail.value,
-        contato: inputContato.value,
-        idade: inputIdade.value,
-        cargo: inputCargo.value,
-        escolaridade: scholarity.value,
-        sexo: sexo.value,
+        nome: form.nome.value,
+        email: form.email.value,
+        contato: form.contato.value,
+        idade: form.idade.value,
+        cargo: form.cargo.value,
+        sexo: document.querySelector('input[name="sexo"]:checked').value,
+        escolaridade: form.escolaridade.value,
     }
-    console.log(personalData)
-});
+    personalInfCont.classList.add('none')
+    profInfoCont.classList.remove('none')
+})
+
+
+// nextPage.addEventListener('click', () => {
+//     const sexo = document.querySelector('input[name="sexo"]:checked')
+//     // profInfoCont.classList.remove('none');
+//     // personalInfCont.classList.add('none');
+//     const personalData = {
+//         nome: inputNome.value,
+//         email: inputEmail.value,
+//         contato: inputContato.value,
+//         idade: inputIdade.value,
+//         cargo: inputCargo.value,
+//         escolaridade: scholarity.value,
+//         sexo: sexo.value,
+//     }
+//     console.log(personalData)
+// });
 
 btnGenerateResume.addEventListener('click', () => {
     contPageAllInfo.classList.remove('none');
